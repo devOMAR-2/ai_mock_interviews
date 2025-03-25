bc7bf887b03d1111798fe0707a4b749ef6ec02b8
devOMAR <omar.alfarraj@yahoo.com>
2025-03-25 19:58:14 +0300

    G

--- constants/index.ts
+++ constants/index.ts
@@ -102,9 +102,9 @@ export const interviewer: CreateAssistantDTO = {
   firstMessage:
     "Hello! Thank you for taking the time to speak with me today. I'm excited to learn more about you and your experience.",
   transcriber: {
-    provider: "openai",
-    model: "GPT-4o Transcribe",
-    language: "ar",
+    provider: "deepgram",
+    model: "nova-2",
+    language: "en",
   },
   voice: {
     provider: "11labs",
